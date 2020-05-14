f60bf1e12f775b417fa5f280604358b26a09d661
Mauricio Llumiquinga <oiciruam73@hotmail.com>
2020-05-13 22:24:32 -0500

    Adding all components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,5 @@
+<div class="Footer">
+    <div class="Footer-container">
+    <div class="Footer-copy">© 2020 DOGSTAGRAM FROM MAURICIO</div>
+    </div>
+</div>
---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="../../public/profile.jpg" alt="">
+    </div>
+    <div class="Profile-info">
+      <h2>vmllc</h2>
+      <span>Mauricio Llumiquinga</span>
+    </div>
+  </div>
+</div>
